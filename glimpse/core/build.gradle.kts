2b412dc97be6a4e95db065a1f123b8ab48543181
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-12-09 00:01:14 +0000

    Publish Android artifacts

--- glimpse/core/build.gradle.kts
+++ glimpse/core/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform")
     id("com.android.library")
+    kotlin("multiplatform")
     id("io.gitlab.arturbosch.detekt") version "1.14.2"
     `maven-publish`
 }
@@ -10,12 +10,16 @@ repositories {
 }
 
 kotlin {
-    android()
+    android {
+        publishLibraryVariants("release")
+    }
+
     jvm(name = "desktop") {
         compilations.all {
             kotlinOptions.jvmTarget = "11"
         }
     }
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -65,43 +69,45 @@ android {
 }
 
 publishing {
-    publications.filterIsInstance<MavenPublication>().forEach { publication ->
-        publication.artifactId = "${project.parent?.name}-${publication.artifactId}"
-        publication.pom {
-            name.set("Glimpse ${project.name.capitalize()}")
-            description.set("OpenGL made simple")
-            url.set("https://glimpse.graphics/")
-            scm {
-                connection.set("scm:git:https://github.com/glimpse-graphics/glimpse.git")
-                developerConnection.set("scm:git:https://github.com/glimpse-graphics/glimpse.git")
-                url.set("https://github.com/glimpse-graphics/glimpse")
-            }
-            licenses {
-                license {
-                    name.set("The Apache Software License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0")
+    publications {
+        afterEvaluate {
+            filterIsInstance<MavenPublication>().forEach { publication ->
+                publication.artifactId = "${project.parent?.name}-${publication.artifactId}"
+                publication.pom {
+                    name.set("Glimpse ${project.name.capitalize()}")
+                    description.set("OpenGL made simple")
+                    url.set("https://glimpse.graphics/")
+                    scm {
+                        connection.set("scm:git:https://github.com/glimpse-graphics/glimpse.git")
+                        developerConnection.set("scm:git:https://github.com/glimpse-graphics/glimpse.git")
+                        url.set("https://github.com/glimpse-graphics/glimpse")
+                    }
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("sczerwinski")
+                            name.set("Slawomir Czerwinski")
+                            email.set("slawomir@czerwinski.it")
+                            url.set("https://czerwinski.it/")
+                        }
+                    }
+                    issueManagement {
+                        system.set("GitHub Issues")
+                        url.set("https://github.com/glimpse-graphics/glimpse/issues")
+                    }
+                    ciManagement {
+                        system.set("GitHub Actions")
+                        url.set("https://github.com/glimpse-graphics/glimpse/actions")
+                    }
                 }
-            }
-            developers {
-                developer {
-                    id.set("sczerwinski")
-                    name.set("Slawomir Czerwinski")
-                    email.set("slawomir@czerwinski.it")
-                    url.set("https://czerwinski.it/")
-                }
-            }
-            issueManagement {
-                system.set("GitHub Issues")
-                url.set("https://github.com/glimpse-graphics/glimpse/issues")
-            }
-            ciManagement {
-                system.set("GitHub Actions")
-                url.set("https://github.com/glimpse-graphics/glimpse/actions")
             }
         }
-    }
 
-    publications {
         repositories {
             maven {
                 url = uri("$buildDir/maven")
