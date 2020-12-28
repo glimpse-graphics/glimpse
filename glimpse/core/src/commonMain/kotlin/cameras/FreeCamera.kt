dca3223c0dd78ec263a78ebb0c5f151c8393c0e0
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-12-28 13:12:37 +0000

    [GL-19] Implement cameras

---
+++ glimpse/core/src/commonMain/kotlin/cameras/FreeCamera.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Slawomir Czerwinski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package graphics.glimpse.cameras
+
+import graphics.glimpse.types.Angle
+import graphics.glimpse.types.Mat4
+import graphics.glimpse.types.TransformationMat4
+import graphics.glimpse.types.Vec3
+
+/**
+ * A freely transformed camera, defined by its [eye] position, and its rotations:
+ * [roll], [pitch], [yaw].
+ */
+data class FreeCamera(
+
+    /**
+     * Position of the camera eye.
+     */
+    override val eye: Vec3,
+
+    /**
+     * Camera roll angle (clockwise/anticlockwise rotation of the camera eye).
+     */
+    val roll: Angle,
+
+    /**
+     * Camera pitch angle (point upwards/downwards).
+     */
+    val pitch: Angle,
+
+    /**
+     * Camera yaw angle (camera heading rotation)
+     */
+    val yaw: Angle
+) : Camera {
+
+    /**
+     * View matrix defined by the camera.
+     */
+    override val viewMatrix: Mat4 = listOf(
+        initialMatrix,
+        TransformationMat4.rotationX(-roll),
+        TransformationMat4.rotationY(pitch),
+        TransformationMat4.rotationZ(-yaw),
+        TransformationMat4.translation(-eye),
+    ).reduce { mat, transformation -> mat * transformation }
+
+    companion object {
+        private val initialMatrix = Mat4(
+            listOf(
+                0f, 0f, -1f, 0f,
+                -1f, 0f, 0f, 0f,
+                0f, 1f, 0f, 0f,
+                0f, 0f, 0f, 1f
+            )
+        )
+    }
+}
---
+++ glimpse/core/src/commonMain/kotlin/cameras/RelativeTargetCamera.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Slawomir Czerwinski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package graphics.glimpse.cameras
+
+import graphics.glimpse.types.Angle
+import graphics.glimpse.types.Mat4
+import graphics.glimpse.types.Vec3
+import graphics.glimpse.types.ViewMat4
+
+/**
+ * A camera pointed at the given [target], and located at the given [distance] from it,
+ * with the given [longitude] and [latitude] angles, while preserving the defined [upVector].
+ */
+data class RelativeTargetCamera(
+
+    /**
+     * Position of the camera target.
+     */
+    val target: Vec3,
+
+    /**
+     * Distance from the target.
+     */
+    val distance: Float,
+
+    /**
+     * Camera longitude relative to the target.
+     */
+    val longitude: Angle,
+
+    /**
+     * Camera latitude relative to the target.
+     */
+    val latitude: Angle,
+
+    /**
+     * Up-vector preserved for the camera.
+     */
+    val upVector: Vec3 = Vec3.unitZ
+) : Camera {
+
+    /**
+     * Position of the camera eye.
+     */
+    override val eye = target + Vec3.fromSphericalCoordinates(distance, longitude, latitude)
+
+    /**
+     * View matrix defined by the camera.
+     */
+    override val viewMatrix: Mat4 =
+        ViewMat4.lookAt(eye, target, upVector)
+}
