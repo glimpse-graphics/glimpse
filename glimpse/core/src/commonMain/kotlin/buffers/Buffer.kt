dce7c2b40bfb6d94259fa733e423434bad03235d
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-12-30 17:34:55 +0000

    Implement buffer with factory

---
+++ glimpse/core/src/commonMain/kotlin/buffers/Buffer.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 Slawomir Czerwinski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package graphics.glimpse.buffers
+
+import graphics.glimpse.GlimpseAdapter
+
+/**
+ * A wrapper for a vertex data buffer.
+ */
+interface Buffer {
+
+    /**
+     * Type of the buffer.
+     */
+    val type: BufferType
+
+    /**
+     * Buffer handle.
+     */
+    val handle: Int
+
+    /**
+     * Tells the given [OpenGL adapter][gl] to use this buffer.
+     */
+    fun use(gl: GlimpseAdapter)
+
+    /**
+     * Tells the given [OpenGL adapter][gl] to dispose this buffer.
+     */
+    fun dispose(gl: GlimpseAdapter)
+
+    /**
+     * A factory for vertex data buffers.
+     */
+    interface Factory {
+
+        /**
+         * Creates new buffers of vertex attributes, defined by given [buffersData].
+         */
+        fun createArrayBuffers(vararg buffersData: FloatBufferData): List<Buffer>
+
+        /**
+         * Creates new buffers of vertex array indices, defined by given [buffersData].
+         */
+        fun createElementArrayBuffers(vararg buffersData: IntBufferData): List<Buffer>
+
+        companion object {
+
+            /**
+             * Returns a new instance of buffer factory.
+             */
+            fun newInstance(gl: GlimpseAdapter): Factory = BufferFactoryImpl(gl)
+        }
+    }
+}
---
+++ glimpse/core/src/commonMain/kotlin/buffers/BufferFactoryImpl.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 Slawomir Czerwinski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package graphics.glimpse.buffers
+
+import graphics.glimpse.GlimpseAdapter
+
+internal class BufferFactoryImpl(private val gl: GlimpseAdapter) : Buffer.Factory {
+
+    override fun createArrayBuffers(vararg buffersData: FloatBufferData): List<Buffer> {
+        val buffersCount = buffersData.size
+
+        val handles = IntArray(size = buffersCount)
+        gl.glGenBuffers(handles)
+
+        return buffersData.mapIndexed { index, bufferData ->
+            gl.glBindBuffer(
+                type = BufferType.ARRAY_BUFFER,
+                bufferHandle = handles[index]
+            )
+            gl.glBufferData(
+                type = BufferType.ARRAY_BUFFER,
+                data = bufferData,
+                usage = BufferUsage.STATIC_DRAW
+            )
+            BufferImpl(
+                type = BufferType.ARRAY_BUFFER,
+                handle = handles[index]
+            )
+        }
+    }
+
+    override fun createElementArrayBuffers(vararg buffersData: IntBufferData): List<Buffer> {
+        val buffersCount = buffersData.size
+
+        val handles = IntArray(size = buffersCount)
+        gl.glGenBuffers(handles)
+
+        return buffersData.mapIndexed { index, bufferData ->
+            gl.glBindBuffer(
+                type = BufferType.ELEMENT_ARRAY_BUFFER,
+                bufferHandle = handles[index]
+            )
+            gl.glBufferData(
+                type = BufferType.ELEMENT_ARRAY_BUFFER,
+                data = bufferData,
+                usage = BufferUsage.STATIC_DRAW
+            )
+            BufferImpl(
+                type = BufferType.ELEMENT_ARRAY_BUFFER,
+                handle = handles[index]
+            )
+        }
+    }
+
+    private data class BufferImpl(
+        override val type: BufferType,
+        override val handle: Int
+    ) : Buffer {
+
+        override fun use(gl: GlimpseAdapter) {
+            gl.glBindBuffer(type, handle)
+        }
+
+        override fun dispose(gl: GlimpseAdapter) {
+            gl.glDeleteBuffers(intArrayOf(handle))
+        }
+    }
+}
