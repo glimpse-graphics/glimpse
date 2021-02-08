67895feb058fcab4ca68a9c04d35dcec8a2e3ffb
Slawomir Czerwinski <slawomir@czerwinski.it>
2021-02-08 21:49:08 +0000

    Convert JogAmp and slf4j dependencies to compileOnly

--- glimpse/core/build.gradle.kts
+++ glimpse/core/build.gradle.kts
@@ -39,9 +39,9 @@ kotlin {
         }
         val desktopMain by getting {
             dependencies {
-                implementation("org.jogamp.jogl:jogl-all-main:2.3.2")
-                implementation("org.jogamp.gluegen:gluegen-rt-main:2.3.2")
-                implementation("org.slf4j:slf4j-api:1.7.30")
+                compileOnly("org.jogamp.jogl:jogl-all-main:2.3.2")
+                compileOnly("org.jogamp.gluegen:gluegen-rt-main:2.3.2")
+                compileOnly("org.slf4j:slf4j-api:1.7.30")
             }
         }
         val desktopTest by getting {
