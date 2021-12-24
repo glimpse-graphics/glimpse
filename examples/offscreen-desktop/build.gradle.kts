b02958edf6bfda230800d098aa7c9e9e777e75a2
Slawomir Czerwinski <slawomir@czerwinski.it>
2021-12-24 17:39:02 +0000

    Upgrade logback to 1.2.10

--- examples/offscreen-desktop/build.gradle.kts
+++ examples/offscreen-desktop/build.gradle.kts
@@ -9,8 +9,8 @@ dependencies {
     implementation("org.jogamp.jogl:jogl-all-main:2.3.2")
     implementation("org.jogamp.gluegen:gluegen-rt-main:2.3.2")
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("ch.qos.logback:logback-core:1.2.9")
-    implementation("ch.qos.logback:logback-classic:1.2.9")
+    implementation("ch.qos.logback:logback-core:1.2.10")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
 }
 
 application {
