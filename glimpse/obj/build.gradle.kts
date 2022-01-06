f92275a933d697f7f53fc8d69cf4cc1d7e39cc4f
Slawomir Czerwinski <slawomir@czerwinski.it>
2022-01-06 22:24:07 +0000

    Upgrade MockK to 1.12.2

--- glimpse/obj/build.gradle.kts
+++ glimpse/obj/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
         val desktopTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-                implementation("io.mockk:mockk:1.12.1")
+                implementation("io.mockk:mockk:1.12.2")
             }
         }
     }
