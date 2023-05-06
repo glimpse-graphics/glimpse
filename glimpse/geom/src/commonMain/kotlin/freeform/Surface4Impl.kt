7ab3282ef1e9fe750f1b3d9a4d8e8b7f02ddfea2
Slawomir Czerwinski <slawomir@czerwinski.it>
2023-05-06 20:06:21 +0100

    Fix surface mesh calculations

--- glimpse/geom/src/commonMain/kotlin/freeform/Surface4Impl.kt
+++ glimpse/geom/src/commonMain/kotlin/freeform/Surface4Impl.kt
@@ -86,10 +86,18 @@ internal data class Surface4Impl<T>(
             }
     }
 
+    init {
+        val expectedControlVerticesSize = gridSize.u * gridSize.v
+        val actualControlVerticesSize = controlVertices.size
+        require(expectedControlVerticesSize == actualControlVerticesSize) {
+            "Invalid number of control points. Expected $expectedControlVerticesSize, got $actualControlVerticesSize"
+        }
+    }
+
     override fun get(parametersValues: Vec2<T>): Vec4<T> =
         getCurve(parametersValues.u)[parametersValues.v]
 
-    private fun getCurve(parameterValueU: T) =
+    private fun getCurve(parameterValueU: T): Curve4<T> =
         Curve4.Builder.getInstance(this.type)
             .ofType(freeformType)
             .withControlPoints(
@@ -129,10 +137,10 @@ internal data class Surface4Impl<T>(
         for (u in 1 until uSize) {
             for (v in 1 until vSize) {
                 val indices = listOf(
-                    uSize * (v - 1) + u,
-                    uSize * (v - 1) + u - 1,
-                    uSize * v + u - 1,
-                    uSize * v + u
+                    vSize * (u - 1) + v,
+                    vSize * (u - 1) + v - 1,
+                    vSize * u + v - 1,
+                    vSize * u + v
                 )
                 builder.addFace(
                     indices = indices.map { index ->
